5dd0dcf617f1b672cab8d6feb132f099527ecbcc
Paganin <paganin.1@osu.edu>
2025-11-12 11:34:55 -0300

    sanity check old esb code and new one

--- test_sim_ESBM/test.R
+++ test_sim_ESBM/test.R
@@ -24,8 +24,9 @@ suppressPackageStartupMessages({
 
 
 ## ---- Source ----
+source(here("R_utilties", "esbm_original.R"))
 source(here("R_utilties", "esbm.R"))
-Rcpp::sourceCpsp(here("R_utilties", "stirling.cpp"))
+Rcpp::sourceCpp(here("R_utilties", "stirling.cpp"))
 
 ## ---- Data ----
 load(here("test_sim_ESBM", "network_1.RData"))  # expects: Y, z_0 (true labels)
@@ -50,22 +51,35 @@ my_z <- seq_len(V)
 Z_DM <- esbm(Y, my_seed, N_iter, "DM", my_z, a = a, b = b,
              beta_DM = beta_dm, H_DM = H_dm)
 
+colnames(Z_DM)
+
+# DM
+Z_DM2 <- esbm_original(Y, my_seed, N_iter, "DM", my_z, a = a, b = b,
+             beta_DM = beta_dm, H_DM = H_dm)
+head(Z_DM) == head(Z_DM2)
+
 ## ---- Collapsed Gibbs sampler (supervised with node attributes) ----
 # Here we use the *true labels* as attributes for the supervised version (as in paper)
+
+## SP - sanity check -- should get the same results
 N_iter <- 2000
 my_seed <- 1
 my_z <- seq_len(V)
-my_x <- as.factor(sample(1:4, V, replace = T))                        # node attribute (supervised)
+# define the vector with node attributes
+my_x <- c(rep(1,20),rep(2,20),rep(3,15),rep(4,15),rep(5,10))
 params <- rep(1, length(unique(my_x)))
 
-# DM + x
 Z_DM_x <- esbm(Y, my_seed, N_iter, "DM", my_z, a = a, b = b,
                beta_DM = beta_dm, H_DM = H_dm, 
-               x = my_x, similarity_fun = similarity_dirichlet, 
+               x = as.factor(my_x), similarity_fun = similarity_dirichlet, 
                sim_args = list(params = params)
               )
 
 # DP + x
-Z_DP_x <- esbm(Y, my_seed, N_iter, "PY", my_z, a = a, b = b,
-               alpha_PY = alpha_dp, sigma_PY = sigma_dp, x = my_x, alpha_xi = my_alpha_xi)
+my_alpha_xi <- rep(1, length(unique(my_x)))
 
+Z_DP_2 <- esbm_original(Y, my_seed, N_iter,"DM", my_z, a = a, b = b,
+               beta_DM = beta_dm, H_DM = H_dm, 
+               x = my_x,  alpha_xi = my_alpha_xi)
+
+tail(Z_DM_x) == tail(Z_DP_2)
