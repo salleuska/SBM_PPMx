c5400116fc56179d76d19dce204cc1cc6194ca88
Paganin <paganin.1@osu.edu>
2026-02-13 15:49:39 -0500

    update eda plots

--- application_brain/0_edaPlots.R
+++ application_brain/0_edaPlots.R
@@ -14,7 +14,7 @@ library(cowplot)   # for arranging plots
 # Load utility plotting function
 source(here("R_utilties", "plottingFunctions.R"))
 # ---------------------------
-# 1) Drop isolated nodes
+# 1) load data, remove nodes with no connections
 # ---------------------------
 # load data
 load(here("application_brain", "consensus_scale33_tau50.RData"))
@@ -22,27 +22,30 @@ load(here("application_brain", "consensus_scale33_tau50.RData"))
 deg <- rowSums(A_bin_tau)
 keep <- deg > 0
 
-message(sprintf("Dropping %d isolated nodes (degree 0) out of %d total.",
+message(sprintf("Dropping %d isolated nodes out of %d total.",
                 sum(!keep), length(keep)))
 
 A <- A_bin_tau[keep, keep, drop = FALSE]
 coords_kept <- coords[keep, , drop = FALSE]
-coords_kept$degree <- deg[keep]
 
 # ---------------------------
-# Fixed anatomical order: Left - Right
+# Decide node order for network plots
 # ---------------------------
-
+# hemisfere labels
 hemi <- ifelse(grepl("^lh\\.", coords_kept$node), "Left",
         ifelse(grepl("^rh\\.", coords_kept$node), "Right",
         ifelse(grepl("^Left-",  coords_kept$node), "Left",
         ifelse(grepl("^Right-", coords_kept$node), "Right", "Other"))))
 
-ord_fixed <- order(hemi, coords_kept$node)
+## if you want to order using labels
+# ord_fixed <- order(hemi, coords_kept$node)
+# hemi_ord <- hemi[ord_fixed]
+
+## ordered using the x coordinate
+ord_fixed <- order(coords_kept$x)
 
 A_ord <- A[ord_fixed, ord_fixed, drop = FALSE]
 coords_ord <- coords_kept[ord_fixed, ]
-hemi_ord <- hemi[ord_fixed]
 
 # ---------------------------
 # 2) Edge list (upper triangle)
@@ -74,15 +77,18 @@ make_view_plot <- function(nodes_df, edges_df, view = c("xy", "xz", "yz"),
   if (view == "xy") {
     xvar <- "x"; yvar <- "y"
     ex1 <- "x1"; ey1 <- "y1"; ex2 <- "x2"; ey2 <- "y2"
-    ttl <- "Axial (x–y)"
+    ttl <- "Axial"
+    xlab <- "x1"; ylab <- "x2"
   } else if (view == "xz") {
     xvar <- "x"; yvar <- "z"
     ex1 <- "x1"; ey1 <- "z1"; ex2 <- "x2"; ey2 <- "z2"
-    ttl <- "Coronal (x–z)"
+    ttl <- "Coronal"
+    xlab <- "x1"; ylab <- "x3"
   } else { # "yz"
     xvar <- "y"; yvar <- "z"
     ex1 <- "y1"; ey1 <- "z1"; ex2 <- "y2"; ey2 <- "z2"
-    ttl <- "Sagittal (y–z)"
+    ttl <- "Sagittal"
+    xlab <- "x2"; ylab <- "x3"
   }
 
   p <- ggplot()
@@ -90,7 +96,8 @@ make_view_plot <- function(nodes_df, edges_df, view = c("xy", "xz", "yz"),
   if (show_edges && nrow(edges_df) > 0) {
     p <- p + geom_segment(
       data = edges_df,
-      aes(x = .data[[ex1]], y = .data[[ey1]], xend = .data[[ex2]], yend = .data[[ey2]]),
+      aes(x = .data[[ex1]], y = .data[[ey1]],
+          xend = .data[[ex2]], yend = .data[[ey2]]),
       alpha = edge_alpha,
       linewidth = edge_lwd
     )
@@ -104,7 +111,7 @@ make_view_plot <- function(nodes_df, edges_df, view = c("xy", "xz", "yz"),
       size = node_size
     ) +
     coord_equal() +
-    labs(title = ttl, x = xvar, y = yvar) +
+    labs(title = ttl, x = xlab, y = ylab) +
     theme_minimal(base_size = 12) +
     theme(
       plot.title = element_text(face = "bold"),
@@ -115,14 +122,17 @@ make_view_plot <- function(nodes_df, edges_df, view = c("xy", "xz", "yz"),
 }
 
 # ---------------------------
-# 4) Make the 3 plots
+# 4) Make  plots
 # ---------------------------
-p_xy <- make_view_plot(coords_kept, edges_df, view = "xy")
-p_xz <- make_view_plot(coords_kept, edges_df, view = "xz")
-p_yz <- make_view_plot(coords_kept, edges_df, view = "yz")
-
-# Display as 1 row of 3
-p_all <- cowplot::plot_grid(p_xy, p_xz, p_yz, nrow = 1)
+p_axial   <- make_view_plot(coords_kept, edges_df, view = "xy")
+p_coronal <- make_view_plot(coords_kept, edges_df, view = "xz")
+p_sagittal <- make_view_plot(coords_kept, edges_df, view = "yz")
+p_all <- cowplot::plot_grid(
+  p_axial,
+  p_coronal,
+  p_sagittal,
+  nrow = 1
+)
 
 # ---------------------------
 # 5) Optional: save to file
@@ -134,10 +144,23 @@ if (!dir.exists(fig_dir)) {
   dir.create(fig_dir, recursive = TRUE)
 }
 
+ggsave(here(fig_dir, "raw_sagittal.png"), p_sagittal, width=5, height=5, dpi=300)
+ggsave(here(fig_dir, "raw_coronal.png"), p_coronal, width=5, height=5, dpi=300)
+ggsave(here(fig_dir, "raw_axial.png"), p_axial, width=5, height=5, dpi=300)
+
+p_all <- plot_grid(
+  p_axial,
+  p_coronal,
+  p_sagittal,
+  nrow = 1,
+  align = "hv",
+  rel_widths = c(1, 1, 1)
+)
+
 # Save the 3-view plot
 ggsave(
   filename = here("application_brain", "figures", "brain_network_3views.png"),
-  plot = gridExtra::arrangeGrob(p_xy, p_xz, p_yz, ncol = 3),
+  plot = p_all,
   width = 12,
   height = 4,
   dpi = 300
@@ -146,15 +169,16 @@ ggsave(
 # ---------------------------
 # 6) Adjacency matrix visualization (ggplot)
 # ---------------------------
-ann_df <- data.frame(
-  hemisphere = factor(hemi_ord, levels = c("Left", "Right"))
+
+plot_matrix_with_annotations(
+  M        = A_ord,
+  type     = "adj",
+  filename = here("application_brain", "figures", "rew_adjacency.pdf")
 )
 
-ann_colors <- list(
-  hemisphere = c(
-    Left  = "#0072B2",
-    Right = "#D55E00"
-  )
+########
+ann_df     = data.frame(
+    hemisphere = factor(hemi, levels = c("Right","Left", "Other"))
 )
 
 
@@ -164,7 +188,6 @@ plot_matrix_with_annotations(
   ann_df   = ann_df, 
   show_cols = "hemisphere",
   ann_colors = list(hemisphere = c(Left="#0072B2", Right="#D55E00", Other="#999999")),
-  gaps_by  = hemi_ord,
+  gaps_by  = hemi,
   filename = here("application_brain", "figures", "adjacency_left_right_order.pdf")
 )
-
